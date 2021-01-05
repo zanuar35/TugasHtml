ec1ae84610a5786c09cac1eb67d860b4d178e0d4
Zanuar Ragmat <rahmatzanuar35@gmail.com>
2021-01-05 19:40:20 +0700

    Pertemuan 10
    Laravel 2

---
+++ practice_laravel3/app/Http/Controllers/MyController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyController extends Controller
+{
+    public function index() {
+        return view('beranda');
+    }
+
+    public function about(){
+        return view('about');
+    }
+
+    public function mahasiswa(){
+        return view('mahasiswa');
+    }
+    
+}
---
+++ practice_laravel3/resources/views/about.blade.php
@@ -0,0 +1,6 @@
+
+@extends('template.base')
+@section('content')
+    <h1>About</h1>
+    <p>Ini adalah praktikum pemrograman Web yang mempelajari Framework PHP yaitu laravel </p>
+@endsection 
